269dddb32d27f45224a969deb97e410a35189c56
leticialbuk <leticia.albuk@gmail.com>
2023-06-13 16:27:46 -0300

    Dapper Contrib

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Blog
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadUsers();
         }
 
         public static void ReadUsers()
