3dc229218d66857d0ba3ef68506fa35fbe9eba64
leticialbuk <leticia.albuk@gmail.com>
2023-06-13 16:06:52 -0300

    criando projeto

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    internal class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost\SQLEXPRESS;Database=Blog;Trusted_Connection=True;TrustServerCertificate=True";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}
