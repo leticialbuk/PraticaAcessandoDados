abab7bd19c69e19448857d324e6fba82eaf1fc7c
leticialbuk <leticia.albuk@gmail.com>
2023-06-13 16:54:53 -0300

    CRUD

--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ namespace Blog
 
         static void Main(string[] args)
         {
-            ReadUsers();
+            //ReadUsers();
+            //ReadUser();
+            //CreateUser();
+            //UpdateUser();
+            DeleteUser();
         }
 
         public static void ReadUsers()
@@ -23,6 +27,65 @@ namespace Blog
                 {
                     Console.WriteLine(user.Name);
                 }
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Name = "Lucas Oliveira",
+                Email = "lucas.si@.com",
+                PasswordHash = "Hash",
+                Bio = "Equipe Rock",
+                Image = "https://..",
+                Slug = "equipe-rock"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado com sucesso!");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Name = "Lucas Oliveira",
+                Email = "lucas.si@.com",
+                PasswordHash = "Hash",
+                Bio = "desenvolvedor fullstack",
+                Image = "https://..",
+                Slug = "equipe-rock"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Atualização realizada com sucesso!");
+            }
+        }
+
+        public static void DeleteUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
+                Console.WriteLine("Usuário deletado com sucesso!");
             }
         }
     }
