aa24a8c4fd2a1cb68cbf3e404e1b53318235084c
leticialbuk <leticia.albuk@gmail.com>
2023-06-16 13:17:20 -0300

    criando um repositório genérico e implementando One to Many

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Blog.Models;
 using Blog.Repositories;
 using Microsoft.Data.SqlClient;
 
@@ -13,28 +13,45 @@ namespace Blog
             var connection = new SqlConnection(CONNECTION_STRING);
             connection.Open();
 
-            //ReadUsers(connection);
-            ReadRoles(connection);
+            ReadUsers(connection);
+            //ReadRoles(connection);
+            //ReadTags(connection);
+
 
             connection.Close();
         }
 
         public static void ReadUsers(SqlConnection connection)
         {
-            var repository = new UserRepository(connection);
-            var users = repository.Get();
+            var repository = new Repository<User>(connection);
+            var items = repository.Get();
 
-            foreach (var user in users)
-                Console.WriteLine(user.Name);
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+                foreach(var role in item.Roles) 
+                {
+                    Console.WriteLine($" - {role.Name}");
+                }
+            }
         }
 
         public static void ReadRoles(SqlConnection connection)
         {
-            var repository = new RoleRepository(connection);
-            var roles = repository.Get();
+            var repository = new Repository<Role>(connection);
+            var items = repository.Get();
 
-            foreach (var role in roles)
-                Console.WriteLine(role.Name);
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
         }
     }
 }
